98ac909b74cb3c4fc0c43008f261db1eb4fa61be
Sahar Lando <saharlan500@gmail.com>
2023-08-06 16:07:18 +0300

    Fix: login / register client behavior

--- client.lua
+++ client.lua
@@ -36,55 +36,59 @@ function WriteAccountToDisk(accountID)
 end
 
 function BankAccountAuth(mode)
+    local labels = {
+        ["login"] = "Logged into account successfully!",
+        ["register"] = "Account created successfully!"
+    }
     local response, username, password, success, user
     if not (mode == "login" or mode == "register") then
         error("Invalid account auth mode")
     end
-    while success ~= true and Drive.isDiskPresent() do
-        Utils.WriteToScreen("terminal", "Please choose a username:", true)
-        username = io.read()
-        Utils.WriteToScreen("terminal", "Please choose a password:")
-        password = io.read()
-        rednet.send(HostID, {
-            ["action"] = mode,
-            ["username"] = username,
-            ["password"] = password
-        })
-        response = Utils.getRednetMessage(HostID)
-        success = response["success"]
-        if not success then
-            Utils.WriteToScreen("terminal", response["message"])
-            sleep(2)
-        end
-    end
-    user = response["account"]
-    Drive.setDiskLabel(username)
-    WriteAccountToDisk(user["id"])
-    if mode == "register" then
-        Utils.WriteToScreen("terminal", "Account created successfully!")
+    Utils.WriteToScreen("terminal", "Please choose a username:", true)
+    username = io.read()
+    Utils.WriteToScreen("terminal", "Please choose a password:")
+    password = io.read()
+    rednet.send(HostID, {
+        ["action"] = mode,
+        ["username"] = username,
+        ["password"] = password
+    })
+    response = Utils.getRednetMessage(HostID)
+    success = response["success"]
+    if not success then
+        Utils.WriteToScreen("terminal", response["message"])
+        sleep(2)
     else
-        Utils.WriteToScreen("terminal", "Logged into account successfully!")
+        user = response["account"]
+        Drive.setDiskLabel(username)
+        WriteAccountToDisk(user["id"])
+        Utils.WriteToScreen("terminal", labels[mode])
+        sleep(1)
     end
-    sleep(1)
     return user
 end
 
 function AccountLoginDialog ()
     -- This function is called if the player needs to create an account.
     local choice, user
-    Utils.WriteToScreen("monitor", "Disk setup required. \nPlease use the computer below.", true)
-    Utils.WriteToScreen("terminal", "Disk is not registered. \nWhat do you want to do now?", true)
-    Utils.WriteToScreen("terminal", "1. Log into an existing account \n2. Create a new account \n3. Eject drive")
-    choice = string.sub(io.read(), 1, 1)
-    if not Drive.isDiskPresent() and (choice == "1" or choice == "2") then
-        print("Disk in not connected!")
-        sleep(1)
-    elseif choice == "1" then
-        BankAccountAuth("login")
-    elseif choice == "2" then
-        BankAccountAuth("register")
-    else
-        Drive.ejectDisk()
+    while user == nil do
+        Utils.WriteToScreen("monitor", "Disk setup required. \nPlease use the computer below.", true)
+        Utils.WriteToScreen("terminal", "Disk is not registered. \nWhat do you want to do now?", true)
+        Utils.WriteToScreen("terminal", "1. Log into an existing account \n2. Create a new account \n3. Eject drive")
+        choice = string.sub(io.read(), 1, 1)
+    
+        if not Drive.isDiskPresent() and (choice == "1" or choice == "2") then
+            Utils.WriteToScreen("terminal", "Disk in not connected!")
+            sleep(1)
+            break
+        elseif choice == "1" then
+            user = BankAccountAuth("login")
+        elseif choice == "2" then
+            user = BankAccountAuth("register")
+        else
+            Drive.ejectDisk()
+            break
+        end
     end
     return user
 end
